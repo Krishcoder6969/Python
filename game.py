2039c1b4552ff1111b185a1127f2811078ddc8d6
Krishcoder6969 <krishant.rajdeo@gmail.com>
2026-02-01 15:18:45 +0400

    rectangle

---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+
+display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Adding image and background image')
+background_image = pygame.transform.scale(
+    pygame.image.load('background')
+)
